4d56ffa98ccd95d44fe47beb4dee1f9a930ae2a1
Tanzila <tanzilakhan74@gmail.com>
2023-03-15 13:02:27 +0530

    hewfb

--- AzureDevops-Feb2023-main/ARMTemplates/MultiStorage/deploy.ps1
+++
@@ -1,5 +0,0 @@
-#Login-AzAccount 
-
-New-AzResourceGroup -Name "ARMTemplates" -Location "CentralIndia"
-
-New-AzResourceGroupDeployment -ResourceGroupName "ARMTemplates" -TemplateFile .\storagetemplate.json -TemplateParameterFile .\parameters.json -Verbose
--- AzureDevops-Feb2023-main/ARMTemplates/MultiVirtualMachine/deploy.ps1
+++
@@ -1,5 +0,0 @@
-#Login-AzAccount 
-
-New-AzResourceGroup -Name "ARMTemplates" -Location "CentralIndia"
-
-New-AzResourceGroupDeployment -ResourceGroupName "ARMTemplates" -TemplateFile .\multivmtemplate.json -TemplateParameterFile .\parameters.json -Verbose
